912d4170cb94f6c26558e2b42d8c02d15833e3a4
BurzoBianca <bburzo18@gmail.com>
2023-07-17 17:25:21 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace WindowsFormsApp_LaAB3_SGBD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridViewParent = new System.Windows.Forms.DataGridView();
+            this.dataGridViewChild = new System.Windows.Forms.DataGridView();
+            this.insertButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewParent)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewChild)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 366);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.connButton_Click);
+            // 
+            // dataGridViewParent
+            // 
+            this.dataGridViewParent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewParent.Location = new System.Drawing.Point(12, 37);
+            this.dataGridViewParent.Name = "dataGridViewParent";
+            this.dataGridViewParent.RowHeadersWidth = 62;
+            this.dataGridViewParent.RowTemplate.Height = 28;
+            this.dataGridViewParent.Size = new System.Drawing.Size(628, 284);
+            this.dataGridViewParent.TabIndex = 1;
+            this.dataGridViewParent.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewParent_CellClick);
+            // 
+            // dataGridViewChild
+            // 
+            this.dataGridViewChild.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewChild.Location = new System.Drawing.Point(646, 37);
+            this.dataGridViewChild.Name = "dataGridViewChild";
+            this.dataGridViewChild.RowHeadersWidth = 62;
+            this.dataGridViewChild.RowTemplate.Height = 28;
+            this.dataGridViewChild.Size = new System.Drawing.Size(628, 284);
+            this.dataGridViewChild.TabIndex = 2;
+            this.dataGridViewChild.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewChild_CellClick);
+            // 
+            // insertButton
+            // 
+            this.insertButton.Location = new System.Drawing.Point(646, 366);
+            this.insertButton.Name = "insertButton";
+            this.insertButton.Size = new System.Drawing.Size(160, 41);
+            this.insertButton.TabIndex = 3;
+            this.insertButton.Text = "Insert";
+            this.insertButton.UseVisualStyleBackColor = true;
+            this.insertButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(1114, 366);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(160, 40);
+            this.deleteButton.TabIndex = 4;
+            this.deleteButton.Text = "Delete ";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(906, 366);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(160, 41);
+            this.updateButton.TabIndex = 5;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1286, 563);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.insertButton);
+            this.Controls.Add(this.dataGridViewChild);
+            this.Controls.Add(this.dataGridViewParent);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "APP";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewParent)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewChild)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridViewParent;
+        private System.Windows.Forms.DataGridView dataGridViewChild;
+        private System.Windows.Forms.Button insertButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button updateButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_LaAB3_SGBD
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = ConfigurationManager.AppSettings.Get("connectionString");
+        private string name_ParentTable = ConfigurationManager.AppSettings.Get("parentTable");
+        private string name_ChildTable = ConfigurationManager.AppSettings.Get("childTable");
+        private string pk_ParentTable;
+        private string pk_ChildTable;
+        private string foreignKey;
+        private string name_foreignKey;
+
+        string currentId_Parent;
+        string currentId_Child;
+
+        DataGridViewRow currentRow_Child;
+
+        DataSet ds;
+        SqlDataAdapter parentTable;
+        SqlDataAdapter childTable;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void setKeys()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                String pkParent_sql;
+                String pkChild_sql;
+                String fk_sql;
+                String fkName_sql;
+
+                pkParent_sql = "DECLARE @pkParent varchar(100);" +
+                    "select @pkParent = C.COLUMN_NAME  FROM  " +
+                    "INFORMATION_SCHEMA.TABLE_CONSTRAINTS T " +
+                    " JOIN INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE C " +
+                    "ON C.CONSTRAINT_NAME = T.CONSTRAINT_NAME " +
+                    "WHERE C.TABLE_NAME = '" + name_ParentTable +
+                    "' and T.CONSTRAINT_TYPE = 'PRIMARY KEY'; " +
+                    "SELECT @pkParent;";
+                pkChild_sql = "DECLARE @pkChild varchar(100);" +
+                    "select C.COLUMN_NAME FROM  " +
+                    "INFORMATION_SCHEMA.TABLE_CONSTRAINTS T " +
+                    " JOIN INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE C " +
+                    "ON C.CONSTRAINT_NAME = T.CONSTRAINT_NAME " +
+                    "WHERE C.TABLE_NAME = '" + name_ChildTable +
+                    "' and T.CONSTRAINT_TYPE = 'PRIMARY KEY'; " +
+                    "SELECT @pkChild;";
+                fk_sql = "DECLARE @fkColumn varchar(100);" +
+                    "SELECT @fkColumn = colpar.name " +
+                    "FROM sys.foreign_keys foreignKey " +
+                    "INNER JOIN sys.foreign_key_columns fkc ON fkc.constraint_object_id = foreignKey.object_id " +
+                    "INNER JOIN sys.tables tpar ON foreignKey.parent_object_id = tpar.object_id " +
+                    "INNER JOIN sys.columns colpar ON fkc.parent_object_id = colpar.object_id AND fkc.parent_column_id = colpar.column_id " +
+                    "INNER JOIN sys.tables tref ON foreignKey.referenced_object_id = tref.object_id " +
+                    "INNER JOIN sys.columns colref ON fkc.referenced_object_id = colref.object_id AND fkc.referenced_column_id = colref.column_id " +
+                    "WHERE tpar.name = '" + name_ChildTable + "' AND tref.name = '" + name_ParentTable + "';" +
+                    "SELECT @fkColumn;";
+                fkName_sql = "select name, OBJECT_NAME(parent_object_id), OBJECT_NAME(referenced_object_id)" +
+                               "from sys.foreign_keys where " +
+                               "parent_object_id = OBJECT_ID('" + name_ChildTable + "'); ";
+
+                try
+                {
+                    connection.Open();
+                    MessageBox.Show("Yay");
+                    SqlCommand cmd1 = new SqlCommand(pkParent_sql, connection);
+                    pk_ParentTable = (string)cmd1.ExecuteScalar();
+                    Console.WriteLine(pk_ParentTable);
+                    SqlCommand cmd2 = new SqlCommand(pkChild_sql, connection);
+                    pk_ChildTable = (string)cmd2.ExecuteScalar();
+                    Console.WriteLine(pk_ChildTable);
+                    SqlCommand cmd3 = new SqlCommand(fk_sql, connection);
+                    foreignKey = (string)cmd3.ExecuteScalar();
+                    Console.WriteLine(foreignKey);
+                    SqlCommand cmd4 = new SqlCommand(fkName_sql, connection);
+                    name_foreignKey = (string)cmd4.ExecuteScalar();
+                    Console.WriteLine(name_foreignKey);
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    connection.Close();
+                }
+                Console.ReadLine();
+            }
+
+
+
+        }
+
+        private void connButton_Click(object sender, EventArgs e)
+        {
+            deleteButton.Enabled = false;
+            updateButton.Enabled = false;
+            setKeys();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    parentTable = new SqlDataAdapter("select * from " + name_ParentTable, connection);
+                    childTable = new SqlDataAdapter("select * from " + name_ChildTable, connection);
+
+                    SqlCommandBuilder cb = new SqlCommandBuilder(parentTable);
+                    cb = new SqlCommandBuilder(childTable);
+
+                    ds = new DataSet();
+                    parentTable.Fill(ds, name_ParentTable);
+                    childTable.Fill(ds, name_ChildTable);
+
+                    DataRelation dr = new DataRelation(name_foreignKey, ds.Tables[name_ParentTable].Columns[pk_ParentTable], ds.Tables[name_ChildTable].Columns[foreignKey]);
+                    ds.Relations.Add(dr);
+
+                    BindingSource bindingSourcesParent = new BindingSource();
+                    BindingSource bindingSourcesChild = new BindingSource();
+
+                    bindingSourcesParent.DataSource = ds;
+                    bindingSourcesParent.DataMember = name_ParentTable;
+                    bindingSourcesChild.DataSource = bindingSourcesParent;
+                    bindingSourcesChild.DataMember = pk_ParentTable;
+
+                    dataGridViewParent.DataSource = bindingSourcesParent;
+                    dataGridViewChild.DataSource = bindingSourcesChild;
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    connection.Close();
+                }
+                Console.ReadLine();
+            }
+        }
+
+        private void dataGridViewParent_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            deleteButton.Enabled = false;
+            updateButton.Enabled = false;
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridViewParent.Rows[e.RowIndex];
+                currentId_Parent = row.Cells[dataGridViewParent.Columns[pk_ParentTable].Index].Value.ToString();
+                getDataChildTable(currentId_Parent);
+            }
+        }
+
+        private void getDataChildTable(string id)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            String sql;
+            SqlCommand command;
+
+            sql = "Select * FROM " + name_ChildTable + " WHERE " + foreignKey + " = @id";
+
+            command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@id", id);
+
+            try
+            {
+                connection.Open();
+
+                childTable = new SqlDataAdapter(command);
+                ds = new DataSet();
+                childTable.Fill(ds, name_ChildTable);
+
+                dataGridViewChild.DataSource = ds.Tables[name_ChildTable].DefaultView;
+
+                dataGridViewChild.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
+                dataGridViewChild.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+                dataGridViewChild.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                connection.Close();
+            }
+            Console.ReadLine();
+
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            String sql;
+            SqlCommand command;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    sql = "delete " + name_ChildTable + " where " + pk_ChildTable + " = @id";
+                    command = new SqlCommand(sql, connection);
+
+                    command.Parameters.AddWithValue("@id", currentId_Child);
+                    Console.WriteLine(currentId_Child);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Information Deleted");
+                    getDataChildTable(currentId_Parent);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    connection.Close();
+                }
+                Console.ReadLine();
+            }
+        }
+
+        private void dataGridViewChild_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            deleteButton.Enabled = true;
+            updateButton.Enabled = true;
+
+            //making foreignKey colum read only
+            dataGridViewChild.Columns[foreignKey].ReadOnly = true;
+
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridViewChild.Rows[e.RowIndex];
+                currentRow_Child = row;
+                currentId_Child = row.Cells[dataGridViewChild.Columns[pk_ChildTable].Index].Value.ToString();
+
+                int nRowIndex = dataGridViewChild.Rows.Count - 1;
+                if (e.RowIndex == nRowIndex)
+                {
+                    deleteButton.Enabled = false;
+                    updateButton.Enabled = false;
+                    insertButton.Enabled = true;
+                }
+                else
+                {
+                    deleteButton.Enabled = true;
+                    updateButton.Enabled = true;
+                    insertButton.Enabled = false;
+                }
+            }
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                childTable.UpdateCommand = new SqlCommandBuilder(childTable).GetUpdateCommand();
+                childTable.Update(ds, name_ChildTable);
+                MessageBox.Show("Updated");
+                getDataChildTable(currentId_Parent);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dataGridViewChild.Rows[currentRow_Child.Index].Cells[foreignKey].Value = currentId_Parent;
+                childTable.UpdateCommand = new SqlCommandBuilder(childTable).GetUpdateCommand();
+                childTable.Update(ds, name_ChildTable);
+                MessageBox.Show("Inserted");
+                getDataChildTable(currentId_Parent);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                dataGridViewChild.Rows.RemoveAt(currentRow_Child.Index);
+            }
+
+        }
+    }
+}
